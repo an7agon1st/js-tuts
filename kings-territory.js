ed21e571b3d434dfe3547db35229abb1dd04c1c6
Tanzilur Rahman <rahman.tanzilur@outlook.com>
2018-12-28 20:46:56 +0530

    Understanding variable declaration and scope of variables

---
+++ kings-territory.js
@@ -0,0 +1,18 @@
+// King's territory problem
+
+//let king = 'me';
+
+if(true){
+  if(true){
+    king='fake shit';
+    console.log(king)
+  }
+}
+if(true){
+  console.log(king)
+}
+
+//prints 'fake shit'
+
+//if you dont initialize a variable with a let or var, it gets initialized with a
+//var, i.e. it is initialized global
