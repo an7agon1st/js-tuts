15ceadcbfc987ab4a96ce660aae2a75413eb1028
Tanzilur Rahman <rahman.tanzilur@outlook.com>
2018-12-28 21:56:20 +0530

    something new, executing code in log

---
+++ templateig.js
@@ -0,0 +1,7 @@
+// ok so something new
+
+//if you have ur log message b/w `` these, and add js code to ${JS CODE HERE},
+//it displays the run CODE
+//pretty cool
+
+console.log(`Ok so this message has a js code ${console.log("...UWU..HI I AM THE JS CODE..UWU...")}<-- here`)
