24a16065e07cf61aec1a24203859bb7f34006c66
Tanzilur Rahman <rahman.tanzilur@outlook.com>
2018-12-28 22:00:26 +0530

    ok some thing weird happens, idk will figure it out later

--- templateig.js
+++ templateig.js
@@ -4,4 +4,4 @@
 //it displays the run CODE
 //pretty cool
 
-console.log(`Ok so this message has a js code ${console.log("...UWU..HI I AM THE JS CODE..UWU...")}<-- here`)
+console.log(`Ok so this message has a js code ${console.log("...UWU..HI I AM THE JS CODE..UWU...")}<-- here`);
