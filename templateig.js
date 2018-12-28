beec8d615da0a4397d3d0d5fba2fc979e710da3b
Alisamar Husain <zrthxn@gmail.com>
2018-12-29 01:11:22 +0530

    Update templateig.js

--- templateig.js
+++ templateig.js
@@ -4,4 +4,13 @@
 //it displays the run CODE
 //pretty cool
 
+/**
+* @author @zrthxn
+* those are called `template strings`
+* generally pretty useful but might have some pitfalls like
+* if you have a variable in which data from some user input is coming
+* and you use `data=${userinput}`
+* this is vulnerable to injection like SQLi
+*/
+
 console.log(`Ok so this message has a js code ${console.log("...UWU..HI I AM THE JS CODE..UWU...")}<-- here`);
