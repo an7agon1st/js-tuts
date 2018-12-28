2a499ba75cd69a5802994ab9c7e8b935f68c6eb2
Alisamar Husain <zrthxn@gmail.com>
2018-12-29 01:16:24 +0530

    Update celcius.js

--- celcius.js
+++ celcius.js
@@ -1,3 +1,14 @@
 let tempInF = -40;
 let tempInC = ((tempInF-32)/9)*5;
 console.log(tempInC);
+
+
+//@author zrthxn
+// idk if you figured out the input taking bit of JS
+// if not, lemme tell
+// since JS is event loop based, it WONT ASK you for input
+// so you need like a package to make it wait and read from the command line
+// that package is called "readline" (only in Node)
+//
+// for the browser, either you can get input from HTML
+// or you can assign values in the dev console in chrome (press F12)
